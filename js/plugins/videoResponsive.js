dec046c70428c49aacf0a1d36bd88885e0ada7be
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-11-18 13:34:03 +0100

    Mise en place de la video responsive

--- js/plugins/videoResponsive.js
+++ js/plugins/videoResponsive.js
@@ -1,38 +1,11 @@
-var w = window.matchMedia("(max-width: 700px)");
-var vid = document.getElementById("vid");
-var source = document.createElement("source");
-source.id = "hvid";
-source.setAttribute("type", "video/mp4");
-vid.appendChild(source);
+var video = $('#myvideo');
 
-if (w.matches) {
-  vid.pause();
-  source.removeAttribute("src");
-  source.setAttribute("src", "https://storage.googleapis.com/coverr-main/mp4/Love-Boat.mp4");
-  vid.load();
-  vid.play();
+var WindowWidth = $(window).width();
+
+if (WindowWidth < 768) {
+    //It is a small screen
+    video.append("<source src='video/videomob.webm' type='video/webm' >");
 } else {
-  vid.pause();
-  source.removeAttribute("src");
-  source.setAttribute("src", "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4");
-  vid.load();
-  vid.play();
+    //It is a big screen or desktop
+    video.append("<source src='video/videodesk.webm' type='video/webm' >");
 }
-
-window.addEventListener("resize", function(){
-var w = window.matchMedia("(max-width: 700px)");
-var vid = document.getElementById("vid");
-var source = document.getElementById("hvid");
-
-if (w.matches) {
-  vid.pause();
-  source.src = "https://storage.googleapis.com/coverr-main/mp4/Love-Boat.mp4";
-  vid.load();
-  vid.play();
-} else {
-  vid.pause();
-  source.src = "https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4";
-  vid.load();
-  vid.play();
-}
-});
