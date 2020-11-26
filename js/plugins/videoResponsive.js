b685fa1110c90cc3f78717860a46fabec47632ce
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-11-26 13:19:04 +0100

    Code Minifié

--- js/plugins/videoResponsive.js
+++ js/plugins/videoResponsive.js
@@ -1,3 +1,1 @@
-var video = $("#myvideo"),
-    WindowWidth = $(window).width();
-WindowWidth < 769 ? video.append("<source src='video/videomob.mp4' type='video/mp4' >") : video.append("<source src='video/videodesk.mp4' type='video/mp4' >");
+var video=$("#myvideo"),WindowWidth=$(window).width();WindowWidth<769?video.append("<source src='video/videomob.mp4' type='video/mp4' >"):video.append("<source src='video/videodesk.mp4' type='video/mp4' >");
