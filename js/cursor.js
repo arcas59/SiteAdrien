54c78a906084112197e23e583a7d99f782104f14
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-12-19 15:11:46 +0100

    correction bug animation cursor v2

--- js/cursor.js
+++ js/cursor.js
@@ -1,12 +1,5 @@
-const cursor = document.querySelector(".cursor");
-document.addEventListener("mousemove", function (e) {
-    return cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;");
-}),
-    document.addEventListener("click", function () {
-        return (
-            cursor.classList.add("expand"),
-            setTimeout(function () {
-                return cursor.classList.remove("expand");
-            }, 500)
-        );
+var cursor = document.querySelector(".cursor");
+    var cursor2 = document.querySelector(".cursor2");
+    document.addEventListener("mousemove",function(e){
+      cursor.style.cssText = cursor2.style.cssText = "left: " + e.clientX + "px; top: " + e.clientY + "px;";
     });
