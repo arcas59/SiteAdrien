43325573b17dd4f7f6b9516ccbafbb1705703726
Adrien Ribeiro <adrienribeiro59200@gmail.com>
2020-12-19 14:00:54 +0100

    Correction bug cursor hover scrollbar

--- js/cursor.js
+++ js/cursor.js
@@ -1,1 +1,12 @@
-const cursor=document.querySelector(".cursor");document.addEventListener("mousemove",function(e){return cursor.setAttribute("style","top: "+(e.pageY-10)+"px; left: "+(e.pageX-10)+"px;")}),document.addEventListener("click",function(){return cursor.classList.add("expand"),setTimeout(function(){return cursor.classList.remove("expand")},500)});
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", function (e) {
+    return cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;");
+}),
+    document.addEventListener("click", function () {
+        return (
+            cursor.classList.add("expand"),
+            setTimeout(function () {
+                return cursor.classList.remove("expand");
+            }, 500)
+        );
+    });
